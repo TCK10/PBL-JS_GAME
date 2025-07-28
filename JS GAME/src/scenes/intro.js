7ede09f955b9dace0fc012a5f7deb0c9bccb895f
TCK10 <tcheekeong1006@gmail.com>
2025-07-28 11:00:12 +0800

    Delete JS GAME directory

--- JS GAME/src/scenes/intro.js
+++
@@ -1,9 +0,0 @@
-import { setBackgroundColor } from "./roomUtils.js";
-
-export function intro(k, roomData) {
-  setBackgroundColor(k, "#10063c");
-
-  const roomLayers = roomData.layers;
-
-  const map = k.add([k.pos(), k.sprite("intro")]);
-}
